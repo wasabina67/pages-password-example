25a29be6bf9804371b6380cd4ceed62f60e4116b
wasabina67 <wasabina67@gmail.com>
2024-08-15 10:35:15 +0900

    Update mkdir.js

--- mkdir.js
+++ mkdir.js
@@ -1,1 +1,7 @@
+const crypto = require('crypto');
+const fs = require('fs').promises;
+const path = require('path');
 
+async function sha256(pw) {
+  return crypto.createHash('sha256').update(pw, 'utf8').digest('hex');
+}
