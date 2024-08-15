906d660ceecd81e5e94e6868308982fbafb0e46c
wasabina67 <wasabina67@gmail.com>
2024-08-15 10:38:32 +0900

    Update mkdir.js

--- mkdir.js
+++ mkdir.js
@@ -8,6 +8,16 @@ async function sha256(pw) {
 
 async function makeHashDirectory(pw) {
   try {
+    const hash = await sha256(pw);
+    const dirPath = path.join('docs', hash);
+    try {
+      await fs.access(dirPath);
+      console.log(`"${dirPath}" already exists.`);
+    } catch {
+      await fs.mkdir(dirPath, { recursive: true });
+      await fs.writeFile(path.join(dirPath, 'index.html'), 'index\n');
+      console.log(`"${dirPath}" is created.`);
+    }
   } catch (error) {
     console.error(error)
   }
