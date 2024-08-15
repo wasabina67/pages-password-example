006cea33365c652edaee67a964a669e7fca74482
wasabina67 <wasabina67@gmail.com>
2024-08-15 10:38:22 +0900

    Update mkdir.js

--- mkdir.js
+++ mkdir.js
@@ -5,3 +5,10 @@ const path = require('path');
 async function sha256(pw) {
   return crypto.createHash('sha256').update(pw, 'utf8').digest('hex');
 }
+
+async function makeHashDirectory(pw) {
+  try {
+  } catch (error) {
+    console.error(error)
+  }
+}
